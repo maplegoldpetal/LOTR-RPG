ddf04794dbd5af476d0253b42d2928f564233b2f
Maple Goldpetal <maplegoldpetal@gmail.com>
2026-06-10 16:43:38 +0000

    added weapon stats and adding a method

--- RPG.py
+++ RPG.py
@@ -1,10 +1,14 @@
 import random
 
 class player:
-    name = input("Insert player name here: ")
+    def __init__(self):
+        pass
+    name = input("Welcome to the Lord of the Rings RPG. Insert player name here: ")
     strength = 0
     speed = 0
     intelligence = 0
+    damagebonus = 0
+    weaponreload = 0
 
     race = input("Please pick a race: Elf, Dwarf, Human or Hobbit: ")
     if race == "Elf":
@@ -26,4 +30,20 @@ class player:
             
     weapon = input("Please pick a weapon: Greatsword (1), Bow (2), Axe(3) or Dagger (4): ")
 
-print(player.name,player.strength,player.speed, player.intelligence)
+    if weapon == "1":
+        damagebonus = "strength"
+        weaponreload = 2
+    if weapon == "2":
+        damagebonus = "intelligence"
+        weaponreload = 1
+    if weapon == "3":
+        damagebonus == "strength"
+        weaponreload = 3
+    if weapon == "4":
+        damagebonus = "speed"
+        weaponreload = 1
+
+    def attack():
+        choice = print("Would you like to attack with hands or a weapon?")
+
+print(f"Hello, {player.name}. We need your help.")
