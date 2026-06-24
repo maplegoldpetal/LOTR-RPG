8c1ebf40e410ec2ed449042add268a8b650748d2
Maple Goldpetal <maplegoldpetal@gmail.com>
2026-06-24 08:01:43 +0000

    I think it works!

--- RPG.py
+++ RPG.py
@@ -3,101 +3,113 @@ import time
 
 class player:
     def __init__(self):
-        pass
-    hp = 50
-    ac = 16
-    name = input("Welcome to the Lord of the Rings RPG. Insert player name here: ")
-    strength = 0
-    speed = 0
-    intelligence = 0
-    damagebonus = 0
-    weaponreload = 0
+        self.hp = 50
+        self.ac = 16
+        self.name = input("Welcome to the Lord of the Rings RPG. Insert player name here: ")
+        self.strength = 0
+        self.speed = 0
+        self.intelligence = 0
+        self.damagebonus = 0
+        self.weaponreload = 0
 
-    race = input("Please pick a race: Elf, Dwarf, Human or Hobbit: ")
-    if race == "Elf":
-        strength = 6
-        speed = 8
-        intelligence = 9
-    if race == "Dwarf":
-        strength = 9
-        speed = 6
-        intelligence = 7
-    if race == "Human":
-        strength = 7
-        speed = 7
-        intelligence = 7
-    if race == "Hobbit":
-        strength = 5
-        speed = 9
-        intelligence =7
+        race = input("Please pick a race: Elf, Dwarf, Human or Hobbit: ")
+        if race == "Elf":
+            self.strength = 6
+            self.speed = 8
+            self.intelligence = 9
+        elif race == "Dwarf":
+            self.strength = 9
+            self.speed = 6
+            self.intelligence = 7
+        elif race == "Human":
+            self.strength = 7
+            self.speed = 7
+            self.intelligence = 7
+        elif race == "Hobbit":
+            self.strength = 5
+            self.speed = 9
+            self.intelligence = 7
             
-    weapon = input("Please pick a weapon: Greatsword (1), Bow (2), Axe(3) or Dagger (4): ")
+        weapon = input("Please pick a weapon: Greatsword (1), Bow (2), Axe(3) or Dagger (4): ")
 
-    if weapon == "1":
-        damagebonus = strength
-        weaponreload = 2
-        hitmessage = "You swing your greatsword with all your might and meet the target!"
-        missmessage = "You swing your greatsword with all your might but miss the target!"
-    if weapon == "2":
-        damagebonus = intelligence
-        weaponreload = 1
-        hitmessage = "You nock an arrow and release it with precision!"
-        missmessage = "You nock an arrow and release it but miss the target!"
-    if weapon == "3":
-        damagebonus = strength
-        weaponreload = 3
-        hitmessage = "You swing your axe with all your might and meet the target!"
-        missmessage = "You swing your axe with all your might but miss the target!"
-    if weapon == "4":
-        damagebonus = speed
-        weaponreload = 1
-        hitmessage = "You strike with your dagger and meet the target!"
-        missmessage = "You strike with your dagger but miss the target!"
+        if weapon == "1":
+            self.damagebonus = self.strength
+            self.weaponreload = 2
+            self.hitmessage = "You swing your greatsword with all your might and meet the target!"
+            self.missmessage = "You swing your greatsword with all your might but miss the target!"
+        elif weapon == "2":
+            self.damagebonus = self.intelligence
+            self.weaponreload = 1
+            self.hitmessage = "You nock an arrow and release it with precision!"
+            self.missmessage = "You nock an arrow and release it but miss the target!"
+        elif weapon == "3":
+            self.damagebonus = self.strength
+            self.weaponreload = 3
+            self.hitmessage = "You swing your axe with all your might and meet the target!"
+            self.missmessage = "You swing your axe with all your might but miss the target!"
+        elif weapon == "4":
+            self.damagebonus = self.speed
+            self.weaponreload = 1
+            self.hitmessage = "You strike with your dagger and meet the target!"
+            self.missmessage = "You strike with your dagger but miss the target!"
 
-    def attack(self):
+    def attack(self, monster):
         weaponchoice = input("Would you like to attack with hands (1) or your weapon (2)? Input your answer here:  ")
         if weaponchoice == "1":
             n = random.randint(1,10)
             x = random.randint(1,5)
-            score = n + player.speed
-            damage = x + player.damagebonus
-            if score >= testmonster.ac:
+            score = n + self.speed
+            damage = x + self.damagebonus
+            if score >= monster.ac:
                 print(self.hitmessage)
-                testmonster.hp -= damage
+                monster.hp -= damage
             else:
                 print(self.missmessage)
         
-        while weaponchoice == "2":
+        elif weaponchoice == "2":
             n = random.randint(1,10)
             x = random.randint(1,10)
-            score = n + player.damagebonus
-            damage = x + player.damagebonus
-            if score >= testmonster.ac:
+            score = n + self.damagebonus
+            damage = x + self.damagebonus
+            if score >= monster.ac:
                 print(self.hitmessage)
-                testmonster.hp -= damage
-                print(f"The monster has {testmonster.hp} health remaining and you are on {player.hp} health.")
-                if testmonster.hp <= 0:
+                monster.hp -= damage
+                print(f"The monster has {monster.hp} health remaining and you are on {self.hp} health.")
+                if monster.hp <= 0:
                     print("You have slain the monster!")
                 else:
                     print("The monster is still alive!")
-                    quit()
             else:
                 print(self.missmessage)
 
 class testmonster:
-    def __init__():
-        pass
-    hp = 50
-    ac = 16
-    def attack(self):
+    def __init__(self):
+        self.hp = 50
+        self.ac = 16
+    
+    def attack(self, player):
         x = random.randint(1,10)
         score = x + 5
         damage = x + 5
-        while score >= player.ac and testmonster.hp > 0:
+        if score >= player.ac and self.hp > 0:
             print("The monster attacks you and hits!")
             player.hp -= damage
-            print(f"The monster has {testmonster.hp} health remaining and you are on {player.hp} health.")
+            print(f"The monster has {self.hp} health remaining and you are on {player.hp} health.")
             time.sleep(1)
+
+# Game execution
+if __name__ == "__main__":
+    player1 = player()
+    monster = testmonster()
+    
+    # Battle loop
+    while player1.hp > 0 and monster.hp > 0:
+        player1.attack(monster)
+        if monster.hp > 0:
+            monster.attack(player1)
+        if player1.hp <= 0:
+            print(f"You have been defeated! Game Over.")
+            break
             if player.hp <= 0:
                 print("You have been slain by the monster!")
                 quit()
